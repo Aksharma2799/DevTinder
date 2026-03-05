ebd91a8589ec4a1f8f8bdb39a3ccff0a1c2f1154
aksharma2799 <rl.ashish01@gmail.com>
2026-03-06 01:27:56 +0530

    Admin Route

--- src/server.js
+++ src/server.js
@@ -1,19 +1,27 @@
-const express = require('express')
+const express = require("express");
 
 const app = express();
 
+app.use("/admin", (req, res, next) => {
+  console.log("Admin auth is checked...");
 
-app.get("/hello", 
-    (req, res, next) => {
-    res.send("welcome to api world");
+  const token = "xyz";
+  const isAdminAuthorized = token === "xyz";
+  if (!isAdminAuthorized) {
+    res.status(401).send("unauthorized request");
+  } else {
     next();
-}),
+  }
+});
 
-app.get("/hello", 
-    (req, res, next) => {
-    res.send("2nd routttt");
-    console.log("1st")
-}),
+app.get("/admin/getAllData", (req, res) => {
+  res.send("All Data send");
+});
+
+app.get("/admin/deleteUser", (req, res) => {
+  res.send("All Data Delete");
+});
+
 app.listen(3000, () => {
-    console.log("server start at port no. 3000")
-})
+  console.log("server start at port no.3000");
+});
